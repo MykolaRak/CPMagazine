502a125b5b182e8de43f6bba36017c0c399fcbbe
Mykola <mykola.rak.89@gmail.com>
2016-03-11 19:26:09 +0200

    templates

--- client/layout/template.js
+++ client/layout/template.js
@@ -3,3 +3,20 @@ Template.image.helpers({
 		return['../img/image.jpg']
 	}
 });
+
+Template.badge.helpers({
+	badges: function () {
+		return['../img/toyota.jpg',
+			   '../img/merc.jpg',
+			   '../img/honda.jpg',
+			   '../img/volks.jpg',
+			   '../img/caddi.jpg',
+			   '../img/ford.jpg']
+	}
+});
+
+Template.text.helpers({
+	texts: function () {
+		return['Toyota', 'Mercedes-Benz', 'Honda', 'Volkswagen', 'Cadillac', 'Ford']
+	}
+});
