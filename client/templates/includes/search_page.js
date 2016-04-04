b87131121624153b2802faa09d7868598511bbd6
Mykola <mykola.rak.89@gmail.com>
2016-04-04 21:03:04 +0300

    db

--- client/templates/includes/search_page.js
+++ client/templates/includes/search_page.js
@@ -1,27 +1,39 @@
 Template.cars.helpers({
-	car: function () {
-		return['car1', 'car2', 'car3', 'car4', 'car5']
-	}
+	cars: function() {
+    return cars.find();
+  }
 });
 
 Template.models.helpers({
-	model: function () {
-		return
-	}
+	model: function() {
+    var model = cars.findOne({name: Session.get('select')});
+    return model.models;
+  }
 });
 
 Template.years.helpers({
-	year: function () {
-		return
-	}
+	year: function() {
+    var model = cars.find({name: Session.get('select')}, {"models.name": 1}).fetch();
+    for(var k in model[0].models) {
+      if(model[0].models.hasOwnProperty(k)) {
+        if (model[0].models[k].name === Session.get('model')) {
+          return model[0].models[k].year;
+        }
+      }
+    }
+  }
 });
 
 Meteor.subscribe('cars');
 
 Template.searchPage.events({
-	'change #brand': function() {
-		var sel = document.getElementById("brand");
-		var select = sel.options[sel.selectedIndex].text;
-	}
+  'change #brand': function() {
+    var sel = document.getElementById("brand");
+    Session.set('select', sel.options[sel.selectedIndex].text)
+  },
+  'change #model': function() {
+    var sel = document.getElementById("model");
+    Session.set('model', sel.options[sel.selectedIndex].text)
+  }
 });
 
--- server/startup.js
+++ server/startup.js
@@ -1,12 +1,36 @@
 if (cars.find().count() === 0) {
 	 cars.insert({
-	    car1: {
-	      model1: {
-	        year: [2012, 2013, 2014]
-	      },
-	      model2: {
-	        year: [2012, 2013, 2014]
-	      }
-	    }
+	     name: 'car1',
+    models: [
+      {
+        name: 'car1model1',
+        year: [2012,2013,2014]
+      },
+      {
+        name: 'car1model2',
+        year: [2012,2013,2014]
+      },
+      {
+        name: 'car1model3',
+        year: [2012,2013,2014]
+      }
+    ]
+  });
+  cars.insert({
+    name: 'car2',
+    models: [
+      {
+        name: 'car2model1',
+        year: [2012,2013,2014]
+      },
+      {
+        name: 'car2model2',
+        year: [2012,2013,2014]
+      },
+      {
+        name: 'car2model3',
+        year: [2012,2013,2014]
+      }
+    ]
   });
 };
