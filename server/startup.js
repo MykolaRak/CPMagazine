e6b2af0511f0ab7e7fedaf55aff05b342fbb1e2d
Mykola <mykola.rak.89@gmail.com>
2016-04-09 15:56:46 +0300

    db

--- server/startup.js
+++ server/startup.js
@@ -6,32 +6,32 @@ if (Cars.find().count() === 0 && Models.find().count() === 0) {
     name: 'Subaru'
   });
   Models.insert({
-    id: Cars.findOne({name: 'Honda'})._id,
+    carId: Cars.findOne({name: 'Honda'})._id,
     name: 'Civic',
     years: [2012,2013,2016]
   });
   Models.insert({
-    id: Cars.findOne({name: 'Honda'})._id,
+    carId: Cars.findOne({name: 'Honda'})._id,
     name: 'Accord',
     years: [2012, 2013, 2016]
   });
   Models.insert({
-    id: Cars.findOne({name: 'Honda'})._id,
+    carId: Cars.findOne({name: 'Honda'})._id,
     name: 'Jazz',
     years: [2012, 2013, 2016]
   });
   Models.insert({
-    id: Cars.findOne({name: 'Subaru'})._id,
+    carId: Cars.findOne({name: 'Subaru'})._id,
     name: 'Impreza',
     years: [2012, 2010, 2016]
   });
   Models.insert({
-    id: Cars.findOne({name: 'Subaru'})._id,
+    carId: Cars.findOne({name: 'Subaru'})._id,
     name: 'Legacy',
     years: [2012, 2011, 2016]
   });
   Models.insert({
-    id: Cars.findOne({name: 'Subaru'})._id,
+    carId: Cars.findOne({name: 'Subaru'})._id,
     name: 'Legacy Outback',
     years: [2012, 2013, 2016]
   });
